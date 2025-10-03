b46884c33405c4adf04106ddf46894d89753871b
Dzrekey001 <dkdzrekey001@gmail.com>
2025-10-03 23:55:09 +0000

    Initial Commit

---
+++ alx-project-0x06/pages/_app.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/layouts/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import store from "@/store/store";
+import { Provider } from "react-redux";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
+}
